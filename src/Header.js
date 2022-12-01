8b8c87e8956b17207f2a02559d736930b6117d08
lunewaii <rebecacarneiropaulaoficial@gmail.com>
2022-11-30 22:22:45 -0300

    não está postando, tenho alterações para fazer

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,16 @@
+import { FirebaseError } from 'firebase/app';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import React, { useState, useEffect } from 'react';
-import { auth, createUserWithEmailAndPassword, updateProfile } from './firebase';
+import { auth, createUserWithEmailAndPassword, updateProfile, storage, db } from './firebase';
+import { getStorage, ref } from "firebase/storage";
 
 function Header(props) {
+
+    const [progress, setProgress] = useState(0); //useState que começa em 0
+
+    const [file, setFile] = useState(null);
+
+    const storage = getStorage();
 
     useEffect(() => {
         
@@ -53,6 +61,7 @@ function Header(props) {
         }).catch((erro) => {
             let {message, code} = erro;
             
+            //aprendi de novo
             switch (code) {
                 case "auth/user-not-found":
                     console.log("num achei ele :(");
@@ -84,6 +93,54 @@ function Header(props) {
         modal.style.display = "none";
     }
 
+    function criarModalUpload(a){
+        a.preventDefault();
+
+        let modal = document.querySelector('.modalUpload');
+
+        modal.style.display = "block";
+    }
+
+    function closeModalUpload(){
+        let modal = document.querySelector('.modalUpload');
+
+        modal.style.display = "none";
+    }
+
+    function uploadPost(a){
+        a.preventDefault();
+        let legenda = document.getElementById('legenda').value;
+        let progressPost = document.getElementById('progressPost');
+
+        // storage.ref = criando referencia pra imagem que vai postar. faz o upload do (file) usando .put
+        const uploadTask = storage.ref('images/${file.name}').put(file);
+
+        uploadTask.on('state_changes', function(snapshot){
+          const progress = Math.round(snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+
+          setProgress(progress);
+        }, function(error){
+            alert('erro');
+        }, function(){
+            storage.ref('images').child(file.name).getDownloadURL().then(function(url){
+                //pega a coleçao posts no bando de dados (db). se existir, vai inserir. se não, vai criar
+                db.collection('posts').add({
+                    legenda: legenda,
+                    image: url,
+                    username: props.user,
+                    // timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                })
+
+                setProgress(0);
+                setFile(null);
+
+                alert('upload realizado com sucesso!');
+
+                document.getElementById('formUpload').reset();
+            })
+        })
+    }
+
     return (
 
         <div className='app'>
@@ -102,6 +159,20 @@ function Header(props) {
                     </div>
                 </div>
 
+                <div className='modalUpload'>
+                    <div className='formUpload'>
+                        <div onClick={() => closeModalUpload()} className='close-modalCreate'>X</div>
+                        <h2>Criar postagem</h2>
+                        <form id='uploadPost' onSubmit={(a) => uploadPost(a)}>
+                            <progress id='progressUpload' value={progress}></progress>
+                            <input id='legenda' type="text" placeholder='Legenda'/>
+                        {/* esse onChange e etc é pra pegar só o último arquivo. aparentemente vou usar muito isso! */}
+                            <input onChange={(a) => setFile(a.target.files[0])} type='file' name='file'/>
+                            <input type="submit" value="Postar"/>
+                        </form>
+                    </div>
+                </div>
+
                 <div className='center'>
                     <div className='headerLogo'>
                         <a href=""><img src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png' /></a>
@@ -112,7 +183,7 @@ function Header(props) {
                         (props.user) ?
                             <div className='header_logado'>
                                 olá {props.user} :)
-                                <a href='#'>postar</a>
+                                <a onClick={(a) => criarModalUpload(a)} href='#'>postar</a>
                             </div>
                             :
                             <div className='headerLoginForm'>
