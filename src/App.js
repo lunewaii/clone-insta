d1527fa0054e2b14ec14c18aded102feac0baadc
lunewaii <rebecacarneiropaulaoficial@gmail.com>
2022-12-09 23:28:42 -0300

    prog versao desatualizada

--- src/App.js
+++ src/App.js
@@ -15,13 +15,33 @@ function App() {
 
   const [user, setUser] = useState();
 
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
+
+    db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(function(snapshot){
+      setPosts(snapshot.docs.map(function(document){
+        return {id:document.id,info:document.data()}
+      }))
+    })
 
   }, [])
 
   return (
     <div className="App">
+
       <Header setUser={setUser} user={user}></Header>
+
+      {
+        posts.map(function(val){
+
+          return (
+            <p>{val.info.legenda}</p>
+          )
+
+        })
+      }
+
     </div>
   );
 }
